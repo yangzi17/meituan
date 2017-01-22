a4c21eda83c691602df02429482149704ecff1cc
pg <pg@pgdeMacBook-Pro.local>
2017-01-22 13:24:39 +0800

    初始化

---
+++ momo/momo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  momo
+//
+//  Created by pg on 22/01/2017.
+//  Copyright © 2017 pg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ momo/momo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  momo
+//
+//  Created by pg on 22/01/2017.
+//  Copyright © 2017 pg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
